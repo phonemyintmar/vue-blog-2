80f662e939713a988fdbfb215097a2c8045a2b58
phonemyintmarr@gmail.com <phonemyintmar@mytel.com.mm>
2023-07-15 11:41:27 +0630

    first

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>this is about</h1>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    backgroundImage: {
+      'background': "url('assets/background.jpg')",
+    },
+    extend: {},
+  },
+  plugins: [
+  
+],
+}
+
