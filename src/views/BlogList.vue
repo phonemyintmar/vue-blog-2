685a29a3fc389514f804668770abd691056faf81
phonemyintmar <106571217+phonemyintmar@users.noreply.github.com>
2024-07-06 23:08:28 +0700

    Update BlogList.vue

--- src/views/BlogList.vue
+++ src/views/BlogList.vue
@@ -3,7 +3,7 @@
   <div class="py-8 px-4 mx-auto max-w-screen lg:py-16 lg:px-6 h-screen">
       <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
           <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blogs</h2>
-          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Currently haven't finished the project, so the blogs are not rendered dynamically yet. Will be updated.</p>
+          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Currently haven't completed the project, so the blogs are not rendered dynamically yet. Will be updated.</p>
       </div> 
       <div class="grid gap-8 lg:grid-cols-2">
           <!-- <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
